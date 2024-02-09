fc8875af710cf8ff86b382d15debd2a9606e621f
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2024-02-09 13:47:18 +0630

    Feat:Add Navbar

--- src/components/Appointment/SmallCalendar.tsx
+++ src/components/Appointment/SmallCalendar.tsx
@@ -32,29 +32,35 @@ export default function SmallCalendar() {
     const nowDay = dayjs().format(format);
     const currDay = day.format(format);
     const slcDay = (daySelectedZ as unknown as Dayjs)?.format(format);
+
+    // Check if the day is a weekend day (Saturday or Sunday)
+    const isWeekend = day.day() === 0 || day.day() === 6;
+
     if (nowDay === currDay) {
-      return "bg-blue-500 rounded-full text-white";
+      return "bg-accent rounded-full text-white";
     } else if (currDay === slcDay) {
-      return "bg-blue-100 rounded-full text-blue-600 font-bold";
+      return "bg-accent/50 rounded-full text-blue-600 font-bold";
+    } else if (isWeekend) {
+      return "text-gray-400 cursor-not-allowed";
     } else {
       return "";
     }
   }
 
   return (
-    <div className="mt-9">
-      <header className="flex justify-between">
-        <p className="text-gray-500 font-bold">
+    <div className="bg-secondary border rounded-xl p-4">
+      <header className="flex justify-between items-center mx-4">
+        <p className="text-white font-bold">
           {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
         </p>
         <div>
           <button onClick={() => setMonthIndexZ(monthIndexZ - 1)}>
-            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            <span className="material-icons-outlined cursor-pointer text-white mx-2">
               <Icon path={mdiChevronLeft} size={1} />
             </span>
           </button>
           <button onClick={() => setMonthIndexZ(monthIndexZ + 1)}>
-            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            <span className="material-icons-outlined cursor-pointer text-white mx-2">
               <Icon path={mdiChevronRight} size={1} />
             </span>
           </button>
@@ -62,7 +68,7 @@ export default function SmallCalendar() {
       </header>
       <div className="grid grid-cols-7 grid-rows-6">
         {currentMonth[0].map((day, i) => (
-          <span key={i} className="text-sm py-1 text-center">
+          <span key={i} className="text-sm py-1 text-center text-gray-600">
             {day.format("dd").charAt(0)}
           </span>
         ))}
@@ -76,7 +82,7 @@ export default function SmallCalendar() {
                   setDaySelectedZ(day);
                   console.log("Day clicked:", day.format("YYYY-MM-DD"));
                 }}
-                className={`py-1 w-full ${getDayClass(day)}`}
+                className={`py-1 w-full ${getDayClass(day)} text-white`}
               >
                 <span className="text-sm">{day.format("D")}</span>
               </button>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className=" h-16 bg-secondary sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <div>
+              <h1 className="text-4xl font-signature ml-2">
+                <a
+                  className="link-underline link-underline-black text-white"
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ACE
+                </a>
+              </h1>
+            </div>
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About Us</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
