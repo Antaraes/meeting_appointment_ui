eeac68228245ef0e936fcefee6524b58a7396317
LinThit27 <linthit2745@gmail.com>
2024-02-15 12:07:01 +0630

    [add] dynamin chart data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import HomePageTable from "@/components/home/HomePageTable";
-import React from "react";
+import useFetch from "@/hooks/useFetch";
+import { getAppointmentsCount } from "@/services/api";
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 
 const data = [
@@ -14,23 +16,65 @@ const data = [
 ];
 
 const page = () => {
+  const { data: fetchedCount } = useFetch("counts", getAppointmentsCount);
+  const [departmentPieChartData, setDepartmentPieChartData] = useState([
+    ["Department", "DepartmentData"],
+  ]);
+  const [roomPieChartData, setRoomPieChartData] = useState([
+    ["Room", "RoomData"],
+  ]);
+  useEffect(() => {
+    if (fetchedCount) {
+      setDepartmentPieChartData((prevData) => [prevData[0]]);
+      setRoomPieChartData((prevData) => [prevData[0]]);
+
+      fetchedCount.data.departmentCount.map(
+        ({
+          departmentName,
+          departmentCount,
+        }: {
+          departmentName: string;
+          departmentCount: string;
+        }) => {
+          setDepartmentPieChartData((prevData) => [
+            ...prevData,
+            [departmentName, departmentCount],
+          ]);
+        },
+      );
+
+      fetchedCount.data.roomCount.map(
+        ({ roomName, roomCount }: { roomName: string; roomCount: string }) => {
+          setRoomPieChartData((prevData) => [
+            ...prevData,
+            [roomName, roomCount],
+          ]);
+        },
+      );
+    }
+  }, [fetchedCount]);
+
   return (
     <div className="h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2 mb-4">
-        <Chart
-          chartType="PieChart"
-          data={data}
-          options={{ title: "test" }}
-          width={"100%"}
-          height={"400px"}
-        />
-        <Chart
-          chartType="PieChart"
-          data={data}
-          options={{ title: "test" }}
-          width={"100%"}
-          height={"400px"}
-        />
+      <div className="mb-4 grid grid-cols-1 md:grid-cols-2">
+        {departmentPieChartData.length > 1 && (
+          <Chart
+            chartType="PieChart"
+            data={departmentPieChartData}
+            options={{ title: "Appointment Count By Department" }}
+            width={"100%"}
+            height={"600px"}
+          />
+        )}
+        {roomPieChartData.length > 1 && (
+          <Chart
+            chartType="PieChart"
+            data={roomPieChartData}
+            options={{ title: "Popular Rooms" }}
+            width={"100%"}
+            height={"600px"}
+          />
+        )}
       </div>
       <HomePageTable />
     </div>
