eece571f0bebf5c30ff89102ab0976ee4fd8bc0f
LinThit27 <linthit2745@gmail.com>
2024-02-20 11:36:43 +0630

    [add] activate working hour

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,23 +8,27 @@ export const getAllAppointment = () => API.get("/appointment");
 export const getAppointmentByRoomID = (roomId: number) =>
   API.get(`/appointment/room/${roomId}`);
 export const createAppointment = (data: any) => API.post("/appointment", data);
-export const comparePassCode = ({ data, id }: { data: any; id: any }) =>
+export const comparePassCode = ({ data, id }: { data: any; id: number }) =>
   API.post(`/appointment/${id}`, data);
-export const updateAppointment = ({data,id}) => API.patch(`/appointment/${id}`, data)
+export const updateAppointment = ({ data, id }) =>
+  API.patch(`/appointment/${id}`, data);
 export const getAppointmentsCount = () => API.get("/appointment/count");
 
 //Working Hours
 export const createWorkingHours = (data: working_hour) =>
   API.post("/working_hour", data);
 export const getAllWorkingHours = () => API.get("/working_hour");
-export const deleteWorkingHours = (id: any) =>
+export const deleteWorkingHours = (id: number) =>
   API.delete(`/working_hour/${id}`);
-export const updateWorkingHours = (id: any, data: any) =>
+export const updateWorkingHours = (id: number, data: any) =>
   API.patch(`/working_hour/${id}`, data);
+export const activateWorkingHours = (id: number) =>
+  API.post(`working_hour/${id}`);
 
 //department
 export const getDepartment = () => API.get("/department");
-export const addDepartment = (departmentData: Department) => API.post("/department", departmentData);
+export const addDepartment = (departmentData: Department) =>
+  API.post("/department", departmentData);
 export const updateDepartment = (id: number, departmentData: Department) =>
   API.patch(`department/${id}`, departmentData);
 export const getDepartmentById = (id: number) => API.get(`/department/${id}`);
