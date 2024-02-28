09240d5b6afce824fd59b139db96ca14ace1832e
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2024-02-28 10:18:34 +0630

    Bug:Typescript error in authZustand and active working hour

--- src/store/AuthZustand.ts
+++ src/store/AuthZustand.ts
@@ -1,27 +1,32 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+// import { create } from "zustand";
+// import { devtools, persist } from "zustand/middleware";
+// type authProp = {
+//   name:string
+// }
+// type TStoreState = {
+//   auth: authProp ;
+//   isAuthenticated: boolean;
+//   setData: () => void;
+// };
 
-type TStoreState = {
-  auth: any;
-  isAuthenticated: boolean;
-  setData: () => void;
-};
+// export const useAuthSlice = create<TStoreState>()(
+//   devtools(
+//     persist(
+//       {
+//         auth: null,
+//         isAuthenticated: false,
+//         setData: () => {},
+//       },
+//       {
+//         name: "auth store",
+//         partialize: (state) =>
+//           Object.fromEntries(
+//             Object.entries(state).filter(
+//               ([key]) => !["user", "token"].includes(key),
+//             ),
+//           ),
+//       },
+//     ),
+//   )
 
-export const useAuthSlice = create<TStoreState>()(
-  persist(
-    {
-      auth: null,
-      isAuthenticated: false,
-      setData: () => {},
-    },
-    {
-      name: "auth store",
-      partialize: (state) =>
-        Object.fromEntries(
-          Object.entries(state).filter(
-            ([key]) => !["user", "token"].includes(key),
-          ),
-        ),
-    },
-  ),
-);
+// );
